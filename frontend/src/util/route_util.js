3094f5a3dc40c684b0a974026d71be37ded825c9
Isaac <iyoon22@gmail.com>
2020-05-11 16:38:09 -0700

    Modify auth route

--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -9,7 +9,7 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
       <Component {...props} />
     ) : (
         // Redirect to home page if the user is authenticated
-        <Redirect to="/" />
+        <Redirect to="/current" />
       )
   )} />
 );
