f2f165fd45acf711419cf10547d10cc8fbcf372c
Isaac <iyoon22@gmail.com>
2020-05-11 11:41:04 -0700

    Change name to correct spelling for Isaac, change entry file to app.js, & add a config folder with keys

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-const mongoose = require("mongoose");
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB succesfully"))
-    .catch(err => console.log(err))
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
-app.get("/",(req,res) => res.send("Hello World"));
 
+app.get("/", (req, res) => res.send("Hello World"));
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
