eb504c6171637e0f0e5f5da97909aee9f4b2b86f
Isaac <iyoon22@gmail.com>
2020-05-11 12:11:20 -0700

    Add a User Registration Route

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+
 
 router.get("/harmonytest", (req, res) => {
   res.json({msg: 'this is the user route'})
 });
 
+
+// Register Route
+router.post('/register', (req, res) => {
+  // Check to make sure nobody has already registered with a duplicate email
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (user) {
+        // Throw a 400 error if the email address already exists
+        return res.status(400).json({ email: "A user has already registered with that email" })
+      } else {
+        // Otherwise create a new user
+        const newUser = new User({
+          handle: req.body.handle,
+          email: req.body.email,
+          password: req.body.password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+})
 
 module.exports = router;
