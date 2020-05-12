15a73ca6290d62d73c9926903de3cd3bd2c3a21f
Zack <zack.musicteacher@gamil.com>
2020-05-11 22:23:51 -0700

    Fix one more styling

--- frontend/src/components/single_grid/grid_col.jsx
+++ frontend/src/components/single_grid/grid_col.jsx
@@ -21,7 +21,7 @@ class GridColumn extends React.Component{
 
   render() {
     const selected = this.state.selected;
-    return (<div className={styles.gridCol}>
+    return (<div>
       {this.props.noteNames.map((note, idx) => (
         <GridItem
           key={idx}
