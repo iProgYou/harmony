702ec7c75d693978ab1487581d67c674ccb225d4
Isaac <iyoon22@gmail.com>
2020-05-13 16:59:30 -0700

    Change indenting for state constant

--- frontend/src/reducers/selectors.js
+++ frontend/src/reducers/selectors.js
@@ -46,49 +46,51 @@ const samplerReadableNotes = (samplerNotes) => {
 }
 
 const state = {
-    entities: {jams: {
-        1: {
-            id: 1,
-            gridIds: [1,2,3,4],
-            beats: 8,
-            hostId: 10
+    entities: {
+        jams: {
+            1: {
+                id: 1,
+                gridIds: [1,2,3,4],
+                beats: 8,
+                hostId: 10
+            }
+        },
+        grids: {
+            1: {
+                id: 1,
+                notes: ["A1","A2","","","A1","A2","",""],
+                instrument: "piano",
+                beats: 8
+            },
+            2: {
+                id: 2,
+                notes: ["","","C#2","B1","","","C#2","B1"],
+                instrument: "keyboard",
+                beats: 8
+            },
+            3: {
+                id: 3,
+                notes: ["A1","","C#2","","A1","","C#2",""],
+                instrument: "bass",
+                beats: 8
+            },
+            4: {
+                id: 3,
+                notes: [
+                    ["A1", "F#2"],
+                    ["A1"],
+                    ["B1", "F#2"],
+                    ["A1"],
+                    ["F#2"],
+                    ["A1"],
+                    ["F#2", "B1"],
+                    ["B1", "E2"]
+                ],
+                instrument: "drums",
+                beats: 8
+            }
         }
-    },
-    grids: {
-        1: {
-            id: 1,
-            notes: ["A1","A2","","","A1","A2","",""],
-            instrument: "piano",
-            beats: 8
-        },
-        2: {
-            id: 2,
-            notes: ["","","C#2","B1","","","C#2","B1"],
-            instrument: "keyboard",
-            beats: 8
-        },
-        3: {
-            id: 3,
-            notes: ["A1","","C#2","","A1","","C#2",""],
-            instrument: "bass",
-            beats: 8
-        },
-        4: {
-            id: 3,
-            notes: [
-                ["A1", "F#2"],
-                ["A1"],
-                ["B1", "F#2"],
-                ["A1"],
-                ["F#2"],
-                ["A1"],
-                ["F#2", "B1"],
-                ["B1", "E2"]
-            ],
-            instrument: "drums",
-            beats: 8
-        }
-    }}
+    }
 };
 
 console.log(samplerReadableNotes(samplerNoteArr(state,state.entities.jams[1])));
