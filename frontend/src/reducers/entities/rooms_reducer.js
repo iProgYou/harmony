9297971e29680d29ff1b7d4c036a8e7d96e720ed
Isaac <iyoon22@gmail.com>
2020-05-13 16:12:29 -0700

    Create rooms reducer

--- frontend/src/reducers/entities/rooms_reducer.js
+++ frontend/src/reducers/entities/rooms_reducer.js
@@ -1,1 +1,21 @@
+import {
+  RECEIVE_ROOMS,
+  RECEIVE_ROOM
+} from "../../actions/room_actions";
+
 // room = id: { id: 0, hostId: 0, jamId:  }
+
+const roomsReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_ROOMS:
+      return Object.assign({}, action.rooms);
+    case RECEIVE_ROOM:
+      return Object.assign({}, action.room);
+    default:
+      return state;
+  }
+};
+
+export default roomsReducer;
