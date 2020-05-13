62808dd97c53dcad044e5ea0087dea7e7ffdc0d4
Isaac <iyoon22@gmail.com>
2020-05-13 16:37:52 -0700

    Create grid reducer

--- frontend/src/actions/grid_actions.js
+++ frontend/src/actions/grid_actions.js
@@ -2,15 +2,20 @@
 export const RECEIVE_GRID = 'RECEIVE_GRID';
 export const RECEIVE_GRIDS = 'RECEIVE_GRIDS';
 
+// grid = id: { id: 0, notes: [...], instrument: 'piano', beats: 0 }
+
+
 export const receiveGrid = grid => ({
   type: RECEIVE_GRID,
   grid
 });
 
-export const receiveGrids = grids => ({
-  type: RECEIVE_GRIDS,
-  grids
-});
+// export const receiveGrids = grids => ({
+//   type: RECEIVE_GRIDS,
+//   grids
+// });
 
-// thunk action?
+// how do we send the state of our grid to other users?
+// pass grid objects through websockets
+
 
--- frontend/src/reducers/entities/grids_reducer.js
+++ frontend/src/reducers/entities/grids_reducer.js
@@ -1,1 +1,21 @@
 // grid = id: { id: 0, notes: [], instrument: "", beats: 0 }
+
+import { RECEIVE_GRID } from "../../actions/grid_actions";
+
+
+const gridReducer = (state = {}, action) => {
+  Object.freeze(state);
+
+  switch (action.type) {
+    case RECEIVE_GRID:
+      return Object.assign({}, state, {
+        [action.grid.id]: action.grid
+      });
+    default:
+      return state;
+  }
+};
+// perhaps add a clear grid action.type
+
+
+export default gridReducer;
