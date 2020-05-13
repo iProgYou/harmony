2cd27105bf8eb939be53bdbcdfcf78609b192d8d
Zack <zack.musicteacher@gamil.com>
2020-05-13 15:10:15 -0700

    One off instruments animatiosn work

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import KeyboardGrid from '../single_grid/keyboard_grid'
 // import PianoGrid from '../single_grid/piano_grid'
-import DrumGrid from '../single_grid/drum_grid'
+// import DrumGrid from '../single_grid/drum_grid'
 // import BassGrid from '../single_grid/bass_grid'
 import MainGrid from '../main_grid/main_grid';
 import styles from './main.module.css'
@@ -11,7 +11,7 @@ class MainPage extends React.Component {
   render() {
     return (
       <div>
-        {/* <MainGrid
+        <MainGrid
           allNotes={[
             [ 'A2', 'A1', 'A4', 'E4' ],
             [ 'F2', 'A4' ],
@@ -34,10 +34,19 @@ class MainPage extends React.Component {
             ["B1", "E2"]
         ]}
           instrument={"drums"}
-        /> */}
-        <DrumGrid
+        />
+        {/* <PianoGrid
           cols={ 8 }
         />
+        <BassGrid
+          cols={ 8 }
+        />
+        <DrumsGrid
+          cols={ 8 }
+        />
+        <KeyboardGrid
+          cols={ 8 }
+        /> */}
 
         <h1 className={styles.blurb}>Make Music</h1>
         <footer className={styles.footer}>
