587b23bbeb0643bffb5118a28f49604c5fae032e
Isaac <iyoon22@gmail.com>
2020-05-14 14:47:46 -0700

    Add styling to signup form

--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -58,47 +58,68 @@ class SignupForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <br />
+      <div className={formStyle.formcontainer}>
+        <div className={formStyle.placeholder}></div>
 
-          <input type="text"
-            value={this.state.email}
-            onChange={this.update('email')}
-            placeholder="Email"
-          />
+        <form onSubmit={this.handleSubmit} className={formStyle.form}>
+          <div className={formStyle.formHeader}>Signup</div>
+   
+          <div>
+            <br />
+            <div className={formStyle.formInputText}>Email</div>
+            <input type="text"
+              value={this.state.email}
+              onChange={this.update('email')}
+              placeholder="Type your email"
+              className={formStyle.formInput}
+            />
 
-          <br />
+            <br />
+            <br />
 
-          <input type="text"
-            value={this.state.handle}
-            onChange={this.update('handle')}
-            placeholder="Handle"
-          />
+            <div className={formStyle.formInputText}>Handle</div>
+            <input type="text"
+              value={this.state.handle}
+              onChange={this.update('handle')}
+              placeholder="Handle"
+              className={formStyle.formInput}
+            />
 
-          <br />
+            <br />
+            <br />
 
-          <input type="password"
-            value={this.state.password}
-            onChange={this.update('password')}
-            placeholder="Password"
-          />
+            <div className={formStyle.formInputText}>Password</div>
+            <input type="password"
+              value={this.state.password}
+              onChange={this.update('password')}
+              placeholder="Password"
+              className={formStyle.formInput}
+            />
 
-          <br />
+            <br />
 
-          <input type="password"
-            value={this.state.password2}
-            onChange={this.update('password2')}
-            placeholder="Confirm Password"
-          />
+            <input type="password"
+              value={this.state.password2}
+              onChange={this.update('password2')}
+              placeholder="Confirm Password"
+              className={formStyle.formInput}
+            />
 
-          <br />
+            <br />
+            <br />
 
-          <input type="submit" value="Submit" />
+            <input 
+              type="submit" 
+              value="Submit"
+              className={formStyle.formSubmitButton}
+            />
 
-          {this.renderErrors()}
-        </div>
-      </form>
+            {this.renderErrors()}
+          </div>
+        </form>
+
+        <div className={formStyle.placeholder}></div>
+      </div>
     );
   }
 }
