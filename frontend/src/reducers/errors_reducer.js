b8e700c749f1ea28d3cce406449d849cf33d3549
Isaac <iyoon22@gmail.com>
2020-05-15 10:58:41 -0700

    Add errors to Rooms

--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
-import SessionErrorsReducer from './session_errors_reducer';
+import sessionErrorsReducer from './session_errors_reducer';
+import roomsErrorsReducer from './rooms_errors_reducer';
 
 export default combineReducers({
-  session: SessionErrorsReducer
+  session: sessionErrorsReducer,
+  rooms: roomsErrorsReducer,
 });
--- models/Jam.js
+++ models/Jam.js
@@ -30,7 +30,7 @@ const JamSchema = new Schema({
     type: Date,
     default: Date.now
   }, 
-  grids: [GridSchema]
+  // grids: [GridSchema]
 });
 
 module.exports = Jam = mongoose.model('Jam', JamSchema);
