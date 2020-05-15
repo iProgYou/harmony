e2590072208a8e56d5c6596c6dd5f265e5332328
Isaac <iyoon22@gmail.com>
2020-05-15 12:43:29 -0700

    Update Room Schema

--- models/Room.js
+++ models/Room.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const MemberSchema = new Schema({
+  member: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  }
+})
+
+
 const RoomSchema = new Schema({
   host: {
     type: Schema.Types.ObjectId,
@@ -20,8 +28,8 @@ const RoomSchema = new Schema({
   // beats or columns, they are the same thing
   savedJams: {
     type: Schema.Types.Mixed
-  }
-
+  }, 
+  memberIds : [MemberSchema]
   // possible future additions
   // 1. members - which should be capped at 4
   // 2. length of jam - all members should have the same jam length
