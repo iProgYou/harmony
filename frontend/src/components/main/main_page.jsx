3fb212d4d48ffa8616d5b932a8aedc4cb6d3026e
Will Sexton <williambsexton@gmail.com>
2020-06-02 13:17:26 -0700

    implement about page

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 // import KeyboardGrid from '../single_grid/keyboard_grid'
 // import PianoGrid from '../single_grid/piano_grid'
 // import DrumGrid from '../single_grid/drum_grid'
+import AboutPage from './about_page'
 import BassGrid from '../single_grid/bass_grid'
 import { ProtectedRoute } from '../../util/route_util';
 import RoomContainer from '../room/room_container';
 import styles from './main.module.css'
-import { Switch,Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 // import { receiveInstrument } from '../../actions/instrument_actions'
 import { clearRoom } from '../../actions/room_actions'
 import { connect } from 'react-redux'
@@ -24,14 +25,19 @@ class MainPage extends React.Component {
     return (
       <div>
         <Switch>
+          <Route path="/about" component={() => <AboutPage />} />
+
           <ProtectedRoute path="/rooms/:roomName/" component={(props) => <RoomContainer socket = {this.socket}  {...props}/>} />
+          
           <Route path="/" component={() => <BassGrid beats={8}/>} />
           {/* <Route to="/:roomName/" render={() => <RoomContainer beats={}/>} /> */}
         </Switch>
 
         <h1 className={styles.blurb}>Make Music</h1>
         <footer className={styles.footer}>
-          Copyright &copy; 2020 Harmony
+          <Link to="/about">About Us</Link>
+          <br />
+          <div>Copyright &copy; 2020 Harmony</div>
         </footer>
       </div>
     );
